a59a724e5e753a8cdd59953adafa39d6b6fab7bc
jiaxin.j.li <jiaxin.j.li@daimler.com>
2021-02-09 14:19:05 +0800

    remove test code

--- jsnext/event.js
+++ jsnext/event.js
@@ -1,5 +1,4 @@
 function addEvent(elem, type, handler) {
-    console.log('trigger event ' + type);
     if (elem.addEventListener) {
         addEvent = (elem, type, handler) => {
             elem.addEventListener(type, handler, false);     
